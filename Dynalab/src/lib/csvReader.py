d0b8b5e8888f4e5cacbf1a1ad8701e64f7a8df03
theo <leray.theo7@gmail.com>
2026-03-15 11:30:58 +0100

    fonction pour lecture/affichage du temps arrondi

--- Dynalab/src/lib/csvReader.py
+++ Dynalab/src/lib/csvReader.py
@@ -35,3 +35,25 @@ def readLengthCSV(type):
             tableau.append(ligne) #...on ajoute la ligne dans la liste ...
 
         return [float(getMinLong(tableau,type)),float(getMaxLong(tableau,type))]
+
+def arrondi(v): 
+    if v % 10 < 5:
+        return (v//10)*10 #dizaine inf
+    else:
+        return ((v + 5)//10)*10 #dizaine sup
+
+def transfo(v):
+    if v < 60:
+        return arrondi(v)
+    elif(v >= 60 and v <= 3600):
+        return arrondi(v//60)
+    else:
+        return v//3600
+    
+def uniteTemps(v):
+    if v < 60:
+        return "s"
+    elif(v >= 60 and v <= 3600):
+        return "m"
+    else:
+        return "h"
